13556c350b1868c0cccc70551ae59f9e84dd105f
johnlsg9909@gmail.com <johnlsg9909@gmail.com>
2022-12-31 16:18:57 +0800

    omp reduction

--- main.c
+++ main.c
@@ -4,30 +4,23 @@
 #define THREAD_COUNT 8
 
 
-static int N = 8;
+static int N = 1000000;
 
 int main()
 {
     int i;
     double avrg, sum=0.0;
-    double local_sum[THREAD_COUNT];
 
     omp_set_num_threads(THREAD_COUNT);
 
     #pragma omp parallel
     {
-        int thread_id = omp_get_thread_num();
-        local_sum[thread_id] = 0;
-
-        #pragma omp for
+        #pragma omp for reduction(+:sum)
         for (i=1; i<N+1; i++) {
-            local_sum[thread_id] += i;
+            sum += i;
         }
     }
 
-    for(i=0; i<THREAD_COUNT; i++){
-        sum+= local_sum[i];
-    }
     avrg = sum/N;
     printf("Average sum is %.1lf", avrg);
 }
